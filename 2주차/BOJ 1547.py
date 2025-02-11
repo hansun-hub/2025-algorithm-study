eb3ea5fcfe6ca9680e9ab5767db99164f68799b0
hansun-hub <hansun33@naver.com>
2025-02-11 22:41:35 +0900

    feat: BOJ 1547번 공

--- 2주차/BOJ 1547.py
+++ 2주차/BOJ 1547.py
@@ -1,3 +1,4 @@
+# 내 코드
 cup= [0]*3
 cup[0]=1
 
@@ -12,3 +13,18 @@ for _ in range(m):
     bounce(x,y)
 index = [i for i,v in enumerate(cup) if v==1]
 print(int(*index)+1)
+
+#############################################
+#개선된 코드
+M = int(input())
+# 컵의 초기 위치 설정 (1번 컵에 공이 있음)
+cups = [1, 2, 3]
+
+for _ in range(M):
+    X, Y = map(int, input().split())
+    # 인덱스를 맞추기 위해 -1을 해줌
+    idx_X, idx_Y = cups.index(X), cups.index(Y)
+    cups[idx_X], cups[idx_Y] = cups[idx_Y], cups[idx_X]
+
+# 공이 있는 컵 찾기 (초기 위치는 1번 컵)
+print(cups.index(1) + 1)

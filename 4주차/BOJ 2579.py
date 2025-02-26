2d776479911ffe12d3f356a71221158c7c0f9fa1
hansun-hub <hansun33@naver.com>
2025-02-26 23:13:48 +0900

    feat: BOJ 2579번 계단오르기

---
+++ 4주차/BOJ 2579.py
@@ -0,0 +1,22 @@
+import sys
+
+def stair_up(n,scores):
+    if n == 1:
+        return scores[0]
+    elif n==2:
+        return scores[0] + scores[1]
+
+    dp = [0] * n
+    dp[0] = scores[0]
+    dp[1] = scores[0] + scores[1]
+    dp[2] = max(scores[0]+scores[2], scores[1]+scores[2])
+
+    for i in range(3,n):
+        dp[i] = max(dp[i-2] + scores[i], dp[i-3] + scores[i-1] + scores[i])
+
+    return dp[n-1]
+
+
+n=int(input())
+scores = list(int(input()) for _ in range(n))
+print(stair_up(n,scores))
